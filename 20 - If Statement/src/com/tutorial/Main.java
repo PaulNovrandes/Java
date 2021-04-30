f6eba9e0ebe234e91150ab5e4c30fb0c47b4eaea
paul novrandes <novrandes.tanuma@gmail.com>
2021-04-30 09:57:21 +0700

    Add files via upload

---
+++ 20 - If Statement/src/com/tutorial/Main.java
@@ -0,0 +1,24 @@
+package com.tutorial;
+
+public class Main {
+
+    public static void main (String[] args){
+
+    // tutorial untuk if statement atau percabangan
+
+        int a = 5;
+        System.out.println("nilai = " + a);
+
+        // ini adalah cabangnya
+        if (a == 10) { // ini adalah ekspresinya
+
+            System.out.println("ini adalah jalur true");// ini adalah statment/pernyataannya
+
+        } else {
+
+            System.out.println("ini adalah jalur false");// ini adalah jalur defaultnya
+        }
+
+        System.out.println("selesai");
+    }
+}
