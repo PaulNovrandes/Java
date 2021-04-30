3205ac9c09e738000060a000af394dd1505512d6
paul novrandes <novrandes.tanuma@gmail.com>
2021-04-30 08:54:51 +0700

    Add files via upload

---
+++ 06 - Printin dan alur eksekusi java/src/com/tutorial/Main.java
@@ -0,0 +1,30 @@
+package com.tutorial;
+
+public class Main {
+
+    public static void main(String[] args){
+
+        System.out.println("Hello teman-teman");
+
+    }
+
+//    public static void main(String[] args){
+//
+//        System.out.println("hallo teman-teman");
+//        System.out.println("baris ke-2 : hallo juga akang pukis");
+//
+//        System.out.print("baris ke-3 : ini adalah bari ketiga");
+//        System.out.print("baris ke-4 : saya melanjutkan baris ketiga");
+//
+//        System.out.print("baris ke-5 : hallo mahmuy???");
+//        System.out.println("baris ke-6 : hallo juga hobloh!!!");
+//
+//        System.out.print("baris ke-7 : hallo mari kita kongkow!!! \n");
+//        System.out.print("baris ke-8 : hallo juga, kongkow ke mana bro?? \n");
+//        System.out.print("baris ke-9 : kita kongkow ke citamiyang \n");
+//
+//        System.out.printf("wiro sableng, naga geni %d", 212);
+//        System.out.print("\n");
+//        System.out.printf("%d", 100);
+//    }
+}
