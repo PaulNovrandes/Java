bab0b059ad90cbed9afc663e3da7563c6e47f8ea
paul novrandes <novrandes.tanuma@gmail.com>
2021-04-30 08:55:43 +0700

    Add files via upload

---
+++ 07 - Memahami String[] args/src/com/tutorial/Main.java
@@ -0,0 +1,19 @@
+package com.tutorial;
+
+public class Main {
+
+    public static void main(String[] args){
+
+//        System.out.println(args[0]);
+//        System.out.println(args[1]);
+//        System.out.println("hai [0]");
+
+        System.out.println(args[0]);
+        System.out.println(args[1]);
+        System.out.println(args[2]);
+        System.out.println("Hallo " + args[3] + " Ganteng");
+        System.out.println("Hallo " + args[4] + " Manise");
+        System.out.println("Hallo kawan-kawan");
+
+    }
+}
