17e03672ff0503d6ee62f35c00bf6bb04ff6f5c7
paul novrandes <novrandes.tanuma@gmail.com>
2021-04-30 08:56:13 +0700

    Add files via upload

---
+++ 08 - Variabel, assignment dan deklarasi/src/com/tutorial/Main.java
@@ -0,0 +1,40 @@
+package com.tutorial;
+
+public class Main {
+
+    public static void main(String[] args){
+
+        //Ini adalah komen
+        /*
+        ini cara membuat
+        komen dengan baris yang
+        lebih dari satu
+         */
+
+        // sekarang kita belajar membuat variabel
+        // tipe data = jenis data dari variabel yang kita buat
+        int a = 10; // Assignment
+        System.out.println("nilai a = " + a);
+
+        a = 20;
+        System.out.println("nilai a baru = " + a);
+
+        //kita akan membuat deklarasi
+        int b, c, d;
+
+        /*
+        ini namanya adalah deklarasi atau menjanjikan
+        bahwa tipe adata dari variabel b, c, dan d adalah
+        sama yaitu int. sedengkan nilai dari variabel tersebut
+        harus dientri secara mandiri.
+         */
+        b = 7;
+        c = 8;
+        d = 9;
+
+        System.out.println("nilai b = " + b);
+        System.out.println("nilai c = " + c);
+        System.out.println("nilai d = " + d);
+
+    }
+}
