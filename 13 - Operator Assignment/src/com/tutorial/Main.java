942cdf36dfc2f032209b2471938fca1e303746ff
paul novrandes <novrandes.tanuma@gmail.com>
2021-04-30 09:45:54 +0700

    Add files via upload

---
+++ 13 - Operator Assignment/src/com/tutorial/Main.java
@@ -0,0 +1,35 @@
+package com.tutorial;
+
+public class Main {
+
+    public static void main(String[] args){
+
+        // Operator Assignment
+
+        // Operator Assignment Penjumlahan (+=)
+        int a = 5;
+        a += 10; // sama dengan 'a = a + 10';
+        System.out.println("nilai a = " + a);
+
+        // Operator Assignment Pengurangan (-=)
+        int b = 10;
+        b -= 5;
+        System.out.println("nilai b = " + b);
+
+        // Operator Assignment Perkalian (*=)
+        int c = 5;
+        c *= 10;
+        System.out.println("nilai c = " + c);
+
+        // Operator Assignment Pembagian (/=)
+        int d = 100;
+        d /= 20;
+        System.out.println("nilai d = " + d);
+
+        // Operator Assignment Modulus atau sisa hasil bagi (%=)
+        int e = 7;
+        e %= 7;
+        System.out.println("nilai e = " + e);
+
+    }
+}
